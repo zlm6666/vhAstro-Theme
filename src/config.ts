8e10ed890bd68de2d72cb0c0afff3d5588345e05
zlm6666 <111101880+zlm6666@users.noreply.github.com>
2025-12-12 18:59:24 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -87,6 +87,7 @@ export default {
     'https://pagead2.googlesyndication.com'
   ],
   // 博客音乐组件解析接口
+  vhMusicApi: 'https://vh-api.4ce.cn/blog/meting',
   // 评论组件（只允许同时开启一个）
   Comment: {
     // Twikoo 评论

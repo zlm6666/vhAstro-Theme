261d3d479be3b98949c8ee11a61c38e234d24a9c
zlm6666 <111101880+zlm6666@users.noreply.github.com>
2025-12-13 16:42:00 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export default {
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
     { text: '朋友', link: '/links', icon: 'Nav_friends' },
-    { text: '圈子', link: '/friends', icon: 'Nav_rss' },
+    { text: '圈子', link: '/friends', icon: 'WebSite_analytics' },
     { text: '动态', link: '/talking', icon: 'Nav_talking' },
     { text: '昔日', link: '/archives', icon: 'Nav_archives' },
     { text: '留言', link: '/message', icon: 'Nav_message' },
